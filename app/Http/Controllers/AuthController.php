0df3232ba8ce6a6f75627c457b994a82f7ab198b
lalelangley <laylanrhdyt@gmail.com>
2025-08-28 08:17:19 +0700

    fix direct halaman

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -11,9 +12,30 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
+  
     public function login(Request $request)
     {
-        // sementara belum isi logika login
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // Ambil user yang sedang login
+            $user = Auth::user();
+
+            // Cek role dan redirect
+            if ($user->role === 'admin') {
+                return redirect()->route('admin.dashboard');
+            } elseif ($user->role === 'asesor') {
+                return redirect()->route('asesor.dashboard');
+            } elseif ($user->role === 'asesi') {
+                return redirect()->route('asesi.dashboard');
+            } else {
+                Auth::logout();
+                return redirect()->route('login')->withErrors('Role tidak dikenali.');
+            }
+        }
+
+        // Kalau gagal login
+        return redirect()->route('login')->withErrors('Email atau password salah.');
     }
 
     public function showRegisterRole()
