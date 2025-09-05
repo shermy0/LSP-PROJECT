d6bf2c5e1cfac3ce06f461338c84202924564a4b
Reno Susanto <renos8157@gmail.com>
2025-09-05 16:07:24 +0700

    fix auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -59,12 +59,12 @@ class AuthController extends Controller
         return redirect()->route('login')->with('success', 'Registrasi berhasil, silakan login');
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
-        Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
         return redirect('/login');
     }
+
 }
