39a5302431af7be5caa149f2f3b077b51f2beab7
Indra <indrajayapermana82@gmail.com>
2025-09-07 09:59:18 +0700

    menambahkan 7 seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,19 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $this->call([
+            UserSeeder::class,
+            AdminSeeder::class,
+            AsesiSeeder::class,
+            AsesorSeeder::class,
+            MigrationsSeeder::class,
+            SkemaSertifikasiSeeder::class,
+            TukSeeder::class,
         ]);
     }
 }
