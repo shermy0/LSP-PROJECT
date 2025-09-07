ffa70643b3e4d232a49b79b4751ce4cdbb2ff4cb
Indra <indrajayapermana82@gmail.com>
2025-09-07 10:32:55 +0700

    menambahkan seeder part 3

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,6 +29,7 @@ class DatabaseSeeder extends Seeder
             SkemaSertifikasiSeeder::class,
             TukSeeder::class,
             AsesorSkemaSeeder::class,
+            UnitKompetensiSeeder::class,
         ]);
     }
 }
