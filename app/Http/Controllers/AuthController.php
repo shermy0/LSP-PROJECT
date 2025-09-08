71410d327d9fe27193c623946499434f3ed1ddec
lalelangley <laylanrhdyt@gmail.com>
2025-09-08 07:59:31 +0700

    Save local changes before pulling

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
             if ($user->role === 'admin') {
                 return redirect()->route('admin.dashboard');
             } elseif ($user->role === 'asesor') {
-                return redirect()->route('asesor.dashboard');
+            return redirect()->route('dashboard.asesor');
             } elseif ($user->role === 'asesi') {
                 return redirect()->route('asesi.dashboard');
             } else {
