9a3b798c8ee8aaec2604a5d69a7516b86f4de6db
Indra <indrajayapermana82@gmail.com>
2025-09-18 12:42:48 +0700

    menyelesaikan masalah database

--- database/migrations/2025_09_06_033055_create_skema_sertifikasi_table.php
+++ database/migrations/2025_09_06_033055_create_skema_sertifikasi_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
             $table->id('id_skema');
             $table->string('nama_skema')->nullable();
             $table->string('kode_skema')->nullable();
+            $table->string('judul_skema')->nullable();
             $table->string('jenjang')->nullable();
             $table->string('bidang_keahlian')->nullable();
             $table->text('deskripsi')->nullable();
