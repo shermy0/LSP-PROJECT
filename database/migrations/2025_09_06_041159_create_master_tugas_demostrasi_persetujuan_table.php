f80be7ca2fc032b08b365467e99e694c06209e2c
Indra <indrajayapermana82@gmail.com>
2025-09-19 09:27:37 +0700

    Update migrations, seeders, and models from demonstrasi 5 tabel

---
+++ database/migrations/2025_09_06_041159_create_master_tugas_demostrasi_persetujuan_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('master_tugas_demostrasi_persetujuan', function (Blueprint $table) {
+            $table->id('id_tugas_demonstrasi_persetujuan');
+            $table->unsignedBigInteger('id_tugas');
+            $table->unsignedBigInteger('id_asesor');
+            $table->date('tgl_ttd_asesor')->nullable();
+            $table->string('ttd_asesor')->nullable();
+
+            $table->foreign('id_tugas')->references('id_tugas')->on('master_tugas_demostrasi')->onDelete('cascade');
+            $table->foreign('id_asesor')->references('id_asesor')->on('asesor')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('demonstrasi_persetujuan');
+    }
+};
