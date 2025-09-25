19a96720da6af63e5221228128db70d3536b50b0
Morax24 <ferrariprayoga6@gmail.com>
2025-09-25 08:25:50 +0700

    Update nomer hp dan nama

---
+++ resources/views/asesi/permohonan/form3.blade.php
@@ -0,0 +1,13 @@
+@extends('master')
+
+@section('title', 'FR.APL.03 - Permohonan Sertifikasi Kompetensi')
+
+@section('konten')
+    <div class="container mt-5 d-flex flex-column justify-content-center align-items-center text-center">
+        <!-- Gambar ilustrasi -->
+        <img src="{{ asset('assets/images/menunggu.png') }}" alt="Menunggu Verifikasi Admin" class="img-fluid mb-4" style="max-width:930px;">
+
+        <!-- Teks
+        <h5 class="fw-semibold text-secondary">Menunggu Verifikasi Admin</h5>-->
+    </div>
+@endsection
