8e9e3d06e3581f6eaed0c1d271fcc27a7acab759
maurabungaalzena <alzenamaurabunga@gmail.com>
2025-09-25 10:22:26 +0700

    blm

--- resources/views/akuntansidankeuanganlembaga2.blade.php
+++ resources/views/akuntansidankeuanganlembaga2.blade.php
@@ -5,7 +5,7 @@
     <h1 class="fw-bold">Form Asesmen</h1>
     <div class="text-center mb-3">
         <h5 class="center-underline">
-             PEMROGRAM JUNIOR (JUNIOR CODER) - AsesKom
+            AKUNTANSI DAN KEUANGAN LEMBAGA 2 - AsesKom
         </h5>
     </div>
 
@@ -25,15 +25,14 @@
     </a>
 </div>
 
-        <!-- Item 3 -->
-<div class="accordion-item mb-2" 
-     onclick="window.location='{{ route('formasesmen.pertanyaanLisan', ['id_skema' => $skema->id_skema]) }}';"
-     style="cursor: pointer;">
-    <h2 class="accordion-header p-3 border rounded">
-        <i class="fa-solid fa-stop me-2"></i> Pertanyaan Esai
-    </h2>
-</div>
 
+
+        <!-- Item 3 -->
+        <div class="accordion-item mb-2">
+            <h2 class="accordion-header p-3 border rounded">
+                <i class="fa-solid fa-stop me-2"></i> Pertanyaan Lisan
+            </h2>
+        </div>
 
         <!-- Item 4 -->
         <div class="accordion-item mb-3">
--- resources/views/juniortechnicalsupport.blade.php
+++ resources/views/juniortechnicalsupport.blade.php
@@ -5,7 +5,7 @@
     <h1 class="fw-bold">Form Asesmen</h1>
     <div class="text-center mb-3">
         <h5 class="center-underline">
-            Sistem Manajemen Asesmen Siswa TJKT - AsesKom
+            JUNIOR TECHNICAL SUPPORT - AsesKom
         </h5>
     </div>
 
@@ -18,14 +18,14 @@
         </div>
         
         <!-- Item 2 -->
-        <div class="accordion-item mb-2">
-            <h2 class="accordion-header p-3 border rounded">
-                <a href="{{ route('formasesmen.pertanyaanEsai', ['id_skema' => $skema->id_skema]) }}" 
-                   class="text-dark text-decoration-none">
-                    <i class="fa-solid fa-stop me-2"></i> Pertanyaan Esai
-                </a>
-            </h2>
-        </div>
+       <div class="accordion-item mb-2">
+    <a href="{{ route('formasesmen.pertanyaanEsai', ['id_skema' => $skema->id_skema]) }}" 
+       class="accordion-header d-block p-3 border rounded text-dark text-decoration-none">
+        <i class="fa-solid fa-stop me-2"></i> Pertanyaan Esai
+    </a>
+</div>
+
+
 
         <!-- Item 3 -->
         <div class="accordion-item mb-2">
