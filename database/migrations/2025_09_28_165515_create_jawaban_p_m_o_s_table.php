5e4480d2b7b354483861c3713a761baf6d3e47c2
lalelangley <laylanrhdyt@gmail.com>
2025-10-23 15:04:12 +0700

    update migration

--- database/migrations/2025_09_28_165515_create_jawaban_p_m_o_s_table.php
+++ database/migrations/2025_09_28_165515_create_jawaban_p_m_o_s_table.php
@@ -26,6 +26,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('jawaban_p_m_o_s');
+        Schema::dropIfExists('jawaban_pmo');
     }
+
 };
