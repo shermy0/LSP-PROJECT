7fc60d1b82ae97486d9145239e7729795e7dbcc6
lalelangley <laylanrhdyt@gmail.com>
2026-03-16 02:04:06 +0700

    perbaikan routes pmo lisan

--- resources/views/akuntansidankeuanganlembaga2.blade.php
+++ resources/views/akuntansidankeuanganlembaga2.blade.php
@@ -28,10 +28,12 @@
 
 
         <!-- Item 3 -->
-        <div class="accordion-item mb-2">
-            <h2 class="accordion-header p-3 border rounded">
-                <i class="fa-solid fa-stop me-2"></i> Pertanyaan Lisan
-            </h2>
+        <div class="accordion-item mb-2" 
+            onclick="window.location='{{ route('formasesmen.pertanyaanLisan', ['id_skema' => $skema->id_skema]) }}';"
+            style="cursor: pointer;">
+                <h2 class="accordion-header p-3 border rounded">
+                    <i class="fa-solid fa-stop me-2"></i> Pertanyaan Lisan
+                </h2>
         </div>
 
         <!-- Item 4 -->
--- resources/views/officeadministative.blade.php
+++ resources/views/officeadministative.blade.php
@@ -25,17 +25,14 @@
     </a>
 </div>
 
-
-
         <!-- Item 3 -->
-               <div class="accordion-item mb-2">
-            <h2 class="accordion-header p-3 border rounded">
-                <a href="{{ route('formasesmen.pertanyaanLisan', ['id_skema' => $skema->id_skema]) }}" 
-                class="text-dark text-decoration-none">
-                    <i class="fa-solid fa-stop me-2"></i> Pertanyaan Lisan
-                </a>
-            </h2>
-        </div>
+<div class="accordion-item mb-2" 
+     onclick="window.location='{{ route('formasesmen.pertanyaanLisan', ['id_skema' => $skema->id_skema]) }}';"
+     style="cursor: pointer;">
+        <h2 class="accordion-header p-3 border rounded">
+            <i class="fa-solid fa-stop me-2"></i> Pertanyaan Lisan
+        </h2>
+</div>
 
         <!-- Item 4 -->
         <div class="accordion-item mb-3">
